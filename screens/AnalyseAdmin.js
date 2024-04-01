dfecfb39098de54fb211db4677a65575197dc6cc
Madow78 <114730776+Madow78@users.noreply.github.com>
2024-04-01 23:01:15 +0200

    Update AnalyseAdmin.js

--- screens/AnalyseAdmin.js
+++ screens/AnalyseAdmin.js
@@ -7,46 +7,44 @@ const AnalyseAdmin = () => {
 
   return (
     <View style={styles.container}>
+      <Text style={styles.intro}>
+        Quelques statistiques intéressantes !
+      </Text>   
+
       <TouchableOpacity 
         style={styles.bouton} 
         onPress={() => navigation.navigate('NbMedoc')}>
-        <Text style={styles.texteBouton}>Statistiques 1</Text>
+        <Text style={styles.texteBouton}>Médicaments signalés</Text>
       </TouchableOpacity>
 
       <TouchableOpacity 
         style={styles.bouton} 
         onPress={() => navigation.navigate('AgeAnalyse')}>
-        <Text style={styles.texteBouton}>Statistiques 2</Text>
+        <Text style={styles.texteBouton}>Âge des utilisateurs</Text>
       </TouchableOpacity>
-
-      <TouchableOpacity 
-        style={styles.bouton} 
-        onPress={() => navigation.navigate('StatistiquePage3')}>
-        <Text style={styles.texteBouton}>Statistiques 3</Text>
-      </TouchableOpacity>
-      {/* Ajoutez plus de boutons si nécessaire */}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     padding: 10,
+    backgroundColor: '#011e36', // Updated to match the dark blue background from LoginScreen
   },
   bouton: {
     width: '80%',
-    padding: 20,
-    margin: 10,
+    padding: 15, // Adjusted for better touch area
+    margin: 15,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#DDD', // Remplacez par votre couleur de fond
+    backgroundColor: '#38d2aa', // Matching the button color from LoginScreen
     borderRadius: 10,
-    elevation: 3, // Ombre pour Android
-    // Ombre pour iOS
-    shadowColor: "#000",
+    elevation: 3, // Shadow for Android
+    shadowColor: "#000", // Shadow for iOS
     shadowOffset: {
       width: 0,
       height: 2,
@@ -56,11 +54,16 @@ const styles = StyleSheet.create({
   },
   texteBouton: {
     fontSize: 18,
-    color: 'black', // Remplacez par votre couleur de texte
+    color: '#ffffff', // Updated to white for consistency with LoginScreen button text
   },
-  navbar: {
-    // Définir le style pour votre navbar ici
-  },
+  intro: {
+    fontSize: 35,
+    textAlign: 'center',
+    color: '#03a770', // Text color from your logo
+    fontWeight: 'bold',
+    lineHeight: 35,
+    marginBottom: 20,
+},
 });
 
 export default AnalyseAdmin;
