84c3f203781f27ecd641df3c94867689cd627a6a
ideuxs <95435985+ideuxs@users.noreply.github.com>
2024-04-01 17:10:01 +0200

    Update AccueilAdmin.js

--- screens/AccueilAdmin.js
+++ screens/AccueilAdmin.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, ScrollView, Button, View } from 'react-native';
+import { auth } from '../firebase';
+import { useNavigation } from '@react-navigation/native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { Image } from 'react-native';
 
-function App() {
+const AccueilAdmin = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World
-        </p>
-      </header>
-    </div>
-  );
+   <View>
+        <Text>Bonjour Admin</Text>
+   </View>
+  )
 }
 
-export default App;
+export default AccueilAdmin;
