0687707fff90cb89c8ea5e846c9ae354bbdc5403
Madow78 <114730776+Madow78@users.noreply.github.com>
2024-04-01 19:57:25 +0200

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-// Importez vos écrans
+// Importez vos Ã©crans
 import LoginScreen from './screens/LoginScreen';
 import ForgotPass from './screens/ForgotPass';
 import AccueilScreen from './screens/AccueilScreen';
@@ -16,6 +16,8 @@ import RechercheCode from './screens/RechercheCode';
 import AccueilAdmin from './screens/AccueilAdmin';
 import SignalementAdmin from './screens/SignalementsAdmin';
 import AnalyseAdmin from './screens/AnalyseAdmin';
+import AnalyseNbMedoc from './screens/AnalyseNbMedocScreen';
+import AnalyseAge from './screens/AnalyseAgeScreen';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -24,15 +26,17 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: true }}>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="Home" component={TabNav}/>
-        <Stack.Screen name="Forgot" component={ForgotPass} />
+        <Stack.Screen name="Login" component={LoginScreen} options={{ title: 'Connexion' }}/>
+        <Stack.Screen name="Register" component={Register} options={{ title: 'Inscription' }} />
+        <Stack.Screen name="Home" component={TabNav} options={{ title: 'Solutions Organix' }}/>
+        <Stack.Screen name="Forgot" component={ForgotPass} options={{ title: 'Mot de passe oublié' }}/>
         <Stack.Screen name="Rechercher" component={RechercheScreen} />
-        <Stack.Screen name="Code" component={RechercheCode} />
-        <Stack.Screen name="Scan" component={ScanQr} />
+        <Stack.Screen name="Code" component={RechercheCode}/>
+        <Stack.Screen name="Scan" component={ScanQr} options={{ title: 'Scanner DataMtrix' }}/>
         <Stack.Screen name="User" component={Profil} />
-        <Stack.Screen name="Admin" component={AdminTabNav} />
+        <Stack.Screen name="Admin" component={AdminTabNav} options={{ title: 'Solutions Organix' }}/>
+        <Stack.Screen name="NbMedoc" component={AnalyseNbMedoc} options={{ title: 'Analyse' }}/>
+        <Stack.Screen name="AgeAnalyse" component={AnalyseAge} options={{ title: 'Analyse' }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -43,7 +47,7 @@ const TabNav = () => (
     <Tab.Screen name="Accueil" component={AccueilScreen} 
       options={{
         title:"Accueil",
-        headerShown:true,
+        headerShown:false,
         tabBarIcon: ({ focused }) => (
           <Image
             source={require('./images/accueil.png')}
@@ -54,7 +58,7 @@ const TabNav = () => (
     <Tab.Screen name="Rechercher" component={RechercheScreen} 
       options={{
         title:"Signaler",
-        headerShown:true, 
+        headerShown:false, 
         tabBarIcon: ({ focused }) => (
           <Image
             source={require('./images/signaler.png')}
@@ -65,7 +69,7 @@ const TabNav = () => (
     <Tab.Screen name="User" component={Profil} 
       options={{
         title:"Profil", 
-        headerShown:true,
+        headerShown:false,
         tabBarIcon: ({ focused }) => (
           <Image
             source={require('./images/utilisateur.png')}
@@ -81,7 +85,7 @@ const AdminTabNav = () => (
     <Tab.Screen name="Accueil" component={AccueilAdmin} 
       options={{
         title:"Accueil",
-        headerShown:true,
+        headerShown:false,
         tabBarIcon: ({ focused }) => (
           <Image
             source={require('./images/accueil.png')}
@@ -92,7 +96,7 @@ const AdminTabNav = () => (
     <Tab.Screen name="Analyse" component={AnalyseAdmin}
       options={{
         title:"Analyse",
-        headerShown:true,
+        headerShown:false,
         tabBarIcon: ({ focused }) => (
           <Image
             source={require('./images/analyse.png')}
@@ -103,7 +107,7 @@ const AdminTabNav = () => (
     <Tab.Screen name="Signalement" component={SignalementAdmin}
       options={{
         title:"Signalement",
-        headerShown:true,
+        headerShown:false,
         tabBarIcon: ({ focused }) => (
           <Image
             source={require('./images/signalements.png')}
